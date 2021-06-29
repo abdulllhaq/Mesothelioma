2047b4c39f2942e824e5f9e16c6c5ac3b986dd76
Skillocity <79521936+Skillocity@users.noreply.github.com>
2021-06-29 16:36:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 from PIL import Image
+from sklearn import preprocessing
 
 
 
@@ -38,6 +39,8 @@ st.write(df.describe())
 x = df.drop(['Outcome'], axis = 1)
 y = df.iloc[:, -1]
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2, random_state = 0)
+lab_enc = preprocessing.LabelEncoder()
+training_scores_encoded = lab_enc.fit_transform(y_train)
 
 
 #User reports
@@ -91,7 +94,7 @@ st.write(user_data)
 
 
 rf  = RandomForestClassifier()
-rf.fit(x_train, y_train)
+rf.fit(x_train, training_scores_encoded)
 user_result = rf.predict(user_data)
 
 
